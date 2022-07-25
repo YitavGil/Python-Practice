bdda3ac0ebb0dbbd13637a8eb9058190e1e47b26
YitavGil <93120542+YitavGil@users.noreply.github.com>
2022-07-25 06:00:20 +0300

    Add files via upload

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -4,7 +4,6 @@ def deal_card():
   random_card = random.choice(cards)
   return random_card
 
-
 user_cards = [deal_card(), deal_card()]
 computer_cards = [deal_card(), deal_card()]
 def init_play():
@@ -18,24 +17,56 @@ def init_play():
   user_score = score(user_cards)
   computer_score = score(computer_cards)
 
-  if user_score == 21:
-    user_score = 0
-  if computer_score == 21:
-    computer_score = 0
+  
+
+  def check_blackjack():
+    if user_score == 21 and computer_score == 21:
+      print("It's a draw!")
+      return
+    elif computer_score > 21 and  user_score <= 21:
+      print("You Win!")
+      return
+    elif computer_score == 21 or user_score < computer_score:
+      print("Computer wins!")
+      return
+    elif user_score == 21 or user_score > computer_score:
+      print("You Win!")
+      return
+    elif user_score > 21:
+      print("Computer wins!")
+      return
+   
+    
+   
+    
+      
   print(f"Your cards: {user_cards}. Yore score {user_score}")
   print(f"Computer first card is {computer_cards[0]}")
 
-  hit = input("Do you wish to add another card? Choose 'y' for yes or 'n' to stay \n")
+  in_game = True
+
+  while in_game:
+    hit = input("Do you wish to add another card? Choose 'y' for yes or 'n' to stay \n")
   
-  if hit == 'y':
-    user_cards.append(deal_card())
-    user_score = score(user_cards)
-    if computer_score < 16:
-      computer_cards.append(deal_card())
-      computer_score = score(computer_cards)
+    if hit == 'y':
+      user_cards.append(deal_card())
+      user_score = score(user_cards)
+      if computer_score < 16:
+        computer_cards.append(deal_card())
+        computer_score = score(computer_cards)
+      print(f"Your cards: {user_cards}. Total score {user_score}")
+    if hit == 'n':
+       if computer_score < 16:
+        computer_cards.append(deal_card())
+        computer_score = score(computer_cards)
+        check_blackjack()
+        in_game = False 
+       
       
   print(f"Your cards: {user_cards}. Total score {user_score}")
+  print(f"Computer cards: {computer_cards}. Total score {computer_score}")
 
  
 
 init_play()
+  
