3d46651e2c9dff6310a8175e21add72d13e08df6
YitavGil <93120542+YitavGil@users.noreply.github.com>
2022-07-28 19:03:47 +0300

    Add files via upload

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -30,6 +30,22 @@ resources = {
     "coffee": 100,
 }
 
+def is_resource_sufficient(order_ingredients):
+    for item in order_ingredients:
+       if order_ingredients[item] >= resources[item]:
+           print(f"Sorry there is not enough {item}.")
+        return False
+    return True
+
+def process_coins():
+    print("Please insert coins.")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.10
+    total += int(input("how many nickles?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
+
+
 profit = 0
 is_on = True
 
@@ -44,3 +60,5 @@ while is_on:
        print(f"Money: ${profit}")
     else:
         drink = MENU[choice]
+       if is_resource_sufficient(drink["ingredients"]):
+           payment = process_coins()
