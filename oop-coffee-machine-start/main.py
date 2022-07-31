bbe14fac8247ea3d990721e43e4c097503dec037
YitavGil <93120542+YitavGil@users.noreply.github.com>
2022-07-31 21:02:46 +0300

    Add files via upload

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -20,4 +20,5 @@ while is_on:
     else:
         drink = menu.find_drink(choice)
         if coffee_maker.is_resource_sufficient(drink):
-            money_machine.make_payment(drink)
+           if money_machine.make_payment(drink.cost):
+               coffee_maker.make_coffee(drink)
