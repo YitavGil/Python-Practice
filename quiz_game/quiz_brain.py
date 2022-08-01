395284d970cb037ac46c834f64c79f45dcf14a22
YitavGil <93120542+YitavGil@users.noreply.github.com>
2022-08-01 06:18:50 +0300

    Add files via upload

--- quiz_game/quiz_brain.py
+++ quiz_game/quiz_brain.py
@@ -3,6 +3,9 @@ class QuizBrain:
         self.question_number = 0
         self.questions_list = questions_list
 
+    def still_has_question(self):
+        return self.question_number < len(self.questions_list)
+
     def next_question(self):
         current_question = self.questions_list[self.question_number]
         self.question_number += 1
