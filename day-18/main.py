890e5311858c8005485c65649281ec540963a728
YitavGil <93120542+YitavGil@users.noreply.github.com>
2022-08-02 05:59:12 +0300

    Add files via upload

--- day-18/main.py
+++ day-18/main.py
@@ -4,11 +4,23 @@ turtle = Turtle()
 turtle.shape('turtle')
 turtle.color('lime green')
 
-# Make square
-for _ in range(4):
-    turtle.forward(150)
-    turtle.right(90)
 
+# Make square
+def make_square():
+    for _ in range(4):
+        turtle.forward(150)
+        turtle.right(90)
+
+
+def dashed_line():
+    for _ in range(5):
+        turtle.forward(20)
+        turtle.penup()
+        turtle.forward(20)
+        turtle.pendown()
+
+
+dashed_line()
 
 
 
