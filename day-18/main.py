60cd9d245dc39da668b26357b1339db0ede95821
YitavGil <93120542+YitavGil@users.noreply.github.com>
2022-08-02 05:44:05 +0300

    Add files via upload

---
+++ day-18/main.py
@@ -0,0 +1,42 @@
+from turtle import Turtle, Screen
+
+turtle = Turtle()
+turtle.shape('turtle')
+turtle.color('lime green')
+
+# Make square
+for _ in range(4):
+    turtle.forward(150)
+    turtle.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
