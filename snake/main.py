37ee8657ef91ed557862963214d95fd03308663d
YitavGil <93120542+YitavGil@users.noreply.github.com>
2022-08-07 05:32:38 +0300

    Add files via upload

--- snake/main.py
+++ snake/main.py
@@ -1,6 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
 import time
+from food import Food
 
 
 screen = Screen()
@@ -10,6 +11,7 @@ screen.title("Classic Snake")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -23,7 +25,8 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 
