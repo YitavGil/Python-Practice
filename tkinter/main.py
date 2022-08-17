2c5312908157cee24730ddd5a1f30c9041302ed8
YitavGil <93120542+YitavGil@users.noreply.github.com>
2022-08-17 05:55:12 +0300

    Add files via upload

--- tkinter/main.py
+++ tkinter/main.py
@@ -1,15 +1,27 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
 
+my_label.config(text="New Text")
 
+#Button
 
+def button_click():
+    my_label.config(text=input.get())
 
+button = Button(text="Click Me", command=button_click)
+button.pack()
+
+#Entry
+
+input = Entry(width=10)
+input.pack()
+input.get()
 
 window.mainloop()
