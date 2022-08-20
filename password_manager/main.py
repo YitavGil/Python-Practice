c44c34a08bbd70d7209f75bda51a8d9c20293470
YitavGil <93120542+YitavGil@users.noreply.github.com>
2022-08-20 12:14:27 +0300

    save data to a file

--- password_manager/main.py
+++ password_manager/main.py
@@ -4,6 +4,17 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save():
+
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -28,9 +39,12 @@ password_label.grid(row=3, column=0)
 # Entries
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
+
 
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "yitavgil@gmail.com")
 
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
@@ -40,6 +54,6 @@ generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=36)
-add_button.grid(row=4, column=1, columnspan=2)
+add_button.grid(row=4, column=1, columnspan=2, command=save)
 
 window.mainloop()
