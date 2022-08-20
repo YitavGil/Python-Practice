448b1163e240e5b182a9aa78f5d10ebde8dc30ce
YitavGil <93120542+YitavGil@users.noreply.github.com>
2022-08-20 12:56:23 +0300

    added popups

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -10,10 +11,19 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
-    with open("data.txt", "a") as data_file:
-        data_file.write(f"{website} | {email} | {password}\n")
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops...", message="Looks like you left empty fields")
+    else:
+        is_ok = messagebox.askokcancel(title=website,
+                                       message=f"These are the entered deatils: \nEmail: {email}\nPassword: {password}"
+                                               f" \n Save these details?")
+
+        if is_ok:
+            with open("data.txt", "a") as data_file:
+                data_file.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -41,7 +51,6 @@ website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
 website_entry.focus()
 
-
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "yitavgil@gmail.com")
@@ -53,7 +62,7 @@ password_entry.grid(row=3, column=1)
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
 
-add_button = Button(text="Add", width=36)
-add_button.grid(row=4, column=1, columnspan=2, command=save)
+add_button = Button(text="Add", width=36, command=save)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
