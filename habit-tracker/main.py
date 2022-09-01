4f388ba74e0edabb1c76565f1d280a5aa0481e7e
YitavGil <93120542+YitavGil@users.noreply.github.com>
2022-09-01 05:54:01 +0300

    Add files via upload

--- habit-tracker/main.py
+++ habit-tracker/main.py
@@ -29,5 +29,15 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers={headers})
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/graph1"
+
+pixel_data = {
+    "date": "20220901",
+    "quantity": "1.2"
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
 print(response.text)
